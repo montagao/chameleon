f731276a448c7083454de625b7416f2850377a90
Istaga <imadethislastnight@gmail.com>
2023-06-25 04:44:14 -0400

    merge

--- packages/nextjs/components/imagecontainer/ImageCard.tsx
+++ packages/nextjs/components/imagecontainer/ImageCard.tsx
@@ -9,13 +9,12 @@ interface ImageCardProps {
 
 export const ImageCard: React.FC<ImageCardProps> = ({ imgLink, altText, onImgChosen, isActive }) => {
   return (
-    <div
-      onClick={() => onImgChosen(imgLink)}
-      className={`max-w-sm rounded overflow-hidden shadow-lg m-2 ${
-        isActive ? "border-4 border-blue-500 shadow-lg" : ""
-      }`}
-    >
-      <div className="aspect-w-1 aspect-h-1">
+    <div onClick={() => onImgChosen(imgLink)} className={`relative max-w-sm rounded`}>
+      <div
+        className={`relative aspect-w-1 aspect-h-1 overflow-hidden shadow-lg m-2 border-4 transition-all duration-500 ease-in-out transform hover:scale-125 hover:z-10 ${
+          isActive ? "border-blue-500" : "border-transparent"
+        }`}
+      >
         <img className="w-full object-cover" src={imgLink} alt={altText} />
       </div>
     </div>
